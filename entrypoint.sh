d04117e76ca0cb11e088fddf66dceaa89dc4f254
ViMatviienko <viktoriia.mataviienko@wdc.com>
2022-08-12 13:05:42 +0200

    modified action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,12 @@ echo "Getting tools"
 
 cd "${GITHUB_WORKSPACE}" || exit 1
 
-git config --global --add safe.directory $GITHUB_WORKSPACE
-
-wget -O - -q https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${CHECKSTYLE_VERSION}/checkstyle-${CHECKSTYLE_VERSION}-all.jar > /checkstyle.jar
+sudo wget -O /checkstyle.jar https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${CHECKSTYLE_VERSION}/checkstyle-${CHECKSTYLE_VERSION}-all.jar?raw=true
 wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
 
 echo "Running check"
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+export REVIEWDOG_INSECURE_SKIP_VERIFY=true
 
 exec java -jar /checkstyle.jar "${INPUT_WORKDIR}" -c "${INPUT_CHECKSTYLE_CONFIG}" -f xml \
  | reviewdog -f=checkstyle \
