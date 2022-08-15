66bae43085e180716d09af86ec4c2a74ea69163a
ViMatviienko <viktoriia.mataviienko@wdc.com>
2022-08-15 09:57:32 +0200

    Revert "modified action with new path to sh"
    This reverts commit 1edcbe15bdf71b9e388abf38497094c8890c63bf.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Getting tools"
 cd "${GITHUB_WORKSPACE}" || exit 1
 
 sudo wget -O /checkstyle.jar https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${CHECKSTYLE_VERSION}/checkstyle-${CHECKSTYLE_VERSION}-all.jar?raw=true
-wget -O - -q https://github.com/ViMatviienko/review_modified/blob/dev/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
+wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
 
 echo "Running check"
 
